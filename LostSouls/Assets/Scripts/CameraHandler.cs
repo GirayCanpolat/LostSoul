071729412d450d06c39cc0b24750dbe8216d34a5
GirayCanpolat <55313567+GirayCanpolat@users.noreply.github.com>
2023-03-18 12:11:04 +0300

    Camera Collesions.
    Camera collesions added.
    
    **Walking Speed ​​should be synchronized with the joystick.

--- LostSouls/Assets/Scripts/CameraHandler.cs
+++ LostSouls/Assets/Scripts/CameraHandler.cs
@@ -12,6 +12,7 @@ namespace GC
         private Transform myTransform;
         private Vector3 cameraTransformPosition;
         private LayerMask ignoreLayers;
+        private Vector3 cameraFollowVelocity = Vector3.zero;
 
         public static CameraHandler signleton;
 
@@ -19,11 +20,16 @@ namespace GC
         public float followSpeed = 0.1f;
         public float pivotSpeed = 0.3f;
 
+        private float targetPosition;  
         private float defaultPosition;
         private float lookAngel;
         private float pivotAngle;
         public float minimumPivot = -35;
         public float maximumPivot = 35;
+
+        public float cameraSphereRadius = 0.2f;
+        public float cameraCollisionOffSet = 0.2f;
+        public float minimumCollisionOffset = 0.2f;
 
 
         private void Awake()
@@ -36,8 +42,10 @@ namespace GC
 
         public void FollowTarget(float delta)
         {
-            Vector3 targetPosition = Vector3.Lerp(myTransform.position, targetTransform.position, delta / followSpeed);
+            Vector3 targetPosition = Vector3.SmoothDamp(myTransform.position, targetTransform.position, ref cameraFollowVelocity, delta / followSpeed);     //Vector3.Lerp(myTransform.position, targetTransform.position, delta / followSpeed);
             myTransform.position = targetPosition;
+
+            handCameraCollisions(delta);
         }
 
         public void HandleCameraRotation(float delta, float mouseXInput, float mouseYInput)
@@ -58,5 +66,27 @@ namespace GC
             cameraPivotTransform.localRotation = targetRotation;
         }
 
+        private void handCameraCollisions(float delta)
+        {
+            targetPosition = defaultPosition;
+            RaycastHit hit;
+            Vector3 direction = cameraTransform.position - cameraPivotTransform.position;
+            direction.Normalize();
+
+            if(Physics.SphereCast(cameraPivotTransform.position, cameraSphereRadius, direction, out hit, Mathf.Abs(targetPosition)
+                , ignoreLayers))
+            {
+                float dis = Vector3.Distance(cameraPivotTransform.position, hit.point);
+                targetPosition = -(dis - cameraCollisionOffSet);
+            }
+
+            if(Mathf.Abs(targetPosition) < minimumCollisionOffset)
+            {
+                targetPosition = -minimumCollisionOffset;
+            }
+
+            cameraTransformPosition.z = Mathf.Lerp(cameraTransform.localPosition.z, targetPosition, delta / 0.2f);
+            cameraTransform.localPosition = cameraTransformPosition;
+        }
     }
 }
